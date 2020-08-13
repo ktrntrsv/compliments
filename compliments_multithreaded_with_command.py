d72043138a2ae9b9cda93f55b1892ce670011784
ktrntrsv <ktrntrsv@gmail.com>
2020-08-13 16:10:52 +0300

    Added compliment command

---
+++ compliments_multithreaded_with_command.py
@@ -0,0 +1,103 @@
+import datetime
+import random
+import time
+from threading import Thread
+
+import requests
+import vk_api
+# from vk_api import
+from vk_api.utils import get_random_id
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+import bot_compliments.configuration as configuration
+from bot_compliments.token_bot import token
+
+
+def authorize():
+    vk_session = vk_api.VkApi(token=token)
+    vk = vk_session.get_api()
+    return vk_session, vk
+
+
+def get_compliment(sex=0):
+    """
+    sex 0 - male
+    sex 1 - female
+    """
+    sex = abs(sex - 1)
+    req = requests.post('http://freegenerator.ru/compliment',
+                        data={'type': 'compliment', 'sex': sex, 'long_val': 1}).json()
+    return req['text']
+
+
+def set_sleep_time(nickname):
+    seconds = random.randint(1, configuration.upper_seconds_bound + 1)
+    now = datetime.datetime.now()
+    delta = datetime.timedelta(seconds=seconds)
+    print(f"[{nickname}] Your compliment will be sent in {seconds} seconds, at {now + delta}")
+    time.sleep(seconds)
+
+
+def sending(vk: vk_api.vk_api.VkApiMethod, member_id: int, sleep: bool = True) -> None:
+    """
+    Sends a message to user
+
+    sleep == True:
+        after waiting(sleeping)
+    sleep == False:
+        right now
+    """
+    nickname = vk.users.get(user_ids=[member_id])[0]['first_name'] + ' ' + \
+               vk.users.get(user_ids=[member_id])[0]['last_name']
+
+    if sleep:
+        set_sleep_time(nickname)
+
+    try:
+        vk.messages.send(
+            user_id=member_id,
+            random_id=get_random_id(),
+            message=get_compliment(sex=vk.users.get(user_id=member_id, fields="sex")[0]['sex'])
+        )
+        print(f"[{nickname}] ok.")
+    except vk_api.exceptions.ApiError:
+        print(f"[{nickname}] I have no access to that user.")
+
+
+def listening(_, vk, vk_session):
+    longpoll = VkLongPoll(vk_session)
+    print("I'm listening")
+
+    while True:
+        try:
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+                    user_info = vk.users.get(user_ids=event.user_id)[0]
+                    message_content: str = event.message
+                    print(f"[{user_info['first_name']} {user_info['last_name']}] I've got a message: "
+                          f"\"{message_content}\"")
+                    if message_content in ("!комплимент", "!Комплимент", "!compliment", "!Compliment"):
+                        sending(vk, user_info["id"], sleep=False)
+        except Exception as ex:
+            print("[!]Exception ", repr(ex))
+            continue
+
+
+# def start_threads():
+#     vk_session, vk = authorize()
+#     while True:
+#         members_id = vk.groups.getMembers(group_id=configuration.group_id)["items"]
+#         threads = [Thread(target=sending, args=(vk, i)) for i in members_id]
+#         threads.append(Thread(target=listening, args=(None, vk_session)))
+#         for i in range(len(threads)):
+#             threads[i].start()
+#         for i in range(len(threads)):
+#             threads[i].join()
+#
+#
+# if __name__ == '__main__':
+#     start_threads()
+
+if __name__ == '__main__':
+    vk_session, vk = authorize()
+    listening(None, vk, vk_session)
